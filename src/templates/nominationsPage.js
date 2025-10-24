eff67b6561f7d3f148543c926dd16ebb4f461ba5
rozsagyenelaw <rozsagyenelaw@yahoo.com>
2025-10-24 15:29:08 +0200

    Delete src/templates directory

--- src/templates/nominationsPage.js
+++
@@ -1,8 +0,0 @@
-export const nominationsPageTemplate = () => {
-  return `NOMINATIONS
-_______________________________________________________________________
-
-This section contains a Confirmation of Names and Fiduciaries that lists the persons appointed to act on your behalf in various fiduciary capacities.
-
-We have included your nomination of a conservator for you or your property in your Power of Attorney, in a document contained in this section, or in both. Your nomination of a conservator may be important if someone other than your successor Trustees or agents petition a court for appointment to take care of you or handle your affairs. As with the other original documents contained in this portfolio, you may choose to replace this original with a copy if you prefer to store the original elsewhere.`;
-};
--- src/templates/personalInformationPage.js
+++
@@ -1,6 +0,0 @@
-export const personalInformationPageTemplate = () => {
-  return `PERSONAL INFORMATION
-_______________________________________________________________________
-
-This section contains copies of the most recent personal and financial information that you have provided to us. If we do additional planning for you in the future, you will need to complete a new questionnaire or update this one.`;
-};
